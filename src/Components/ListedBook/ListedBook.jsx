598c966e02a612620b36f930b8497f8b50c9b3eb
Mujahidul Isalm Rifat <mirifat4541@gmail.com>
2024-11-06 16:25:57 +0600

    some changes

--- src/Components/ListedBook/ListedBook.jsx
+++ src/Components/ListedBook/ListedBook.jsx
@@ -11,6 +11,19 @@ export default function ListedBook() {
     const [readList, setReadList] = useState([]);
     const allBooksData = useLoaderData();
 
+    const [short, setShort] = useState('')
+
+    const handleShort = shortType => {
+        setShort(shortType)
+
+        if (shortType === 'Number of pages') {
+            const shortedReadList = [...readList].short((a, b) => a.totalPages - b.totalPages);
+            setReadList(shortedReadList)
+        }
+    }
+
+
+
     useEffect(() => {
         const loadMarkAsRead = getTheMarkAsRead()
         // const loadWishList=getAddToWishList()
@@ -27,6 +40,21 @@ export default function ListedBook() {
         <div>
             <h2 className="text-3xl my-8">Listed Book</h2>
 
+
+            <div className='flex justify-center'>
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn m-1">
+                        {
+                            short ? `Short By :${short}` : 'short By'
+                        }
+                    </div>
+                    <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                        <li onClick={() => { handleShort('Rating') }}><a>Ratings</a></li>
+                        <li onClick={() => { handleShort('Number of pages') }}><a>number of pages</a></li>
+                    </ul>
+                </div>
+            </div>
+
             <Tabs>
                 <TabList>
                     <Tab>Read Books</Tab>
@@ -36,7 +64,7 @@ export default function ListedBook() {
                 <TabPanel>
                     <h2>Read Books:{readList.length}</h2>
                     {
-                        readList.map(book=><ListedBookRead key={book.bookId} book={book}></ListedBookRead>)
+                        readList.map(book => <ListedBookRead key={book.bookId} book={book}></ListedBookRead>)
                     }
                 </TabPanel>
                 <TabPanel>
--- src/Components/ListedReadBook/ListedBookRead.jsx
+++ src/Components/ListedReadBook/ListedBookRead.jsx
@@ -11,7 +11,7 @@ export default function ListedBookRead({ book }) {
                         src={image}
                         className=" h-52 rounded-lg shadow-2xl" />
                     <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
+                        <h1 className="text-5xl font-bold">{bookName}</h1>
                         <p className="py-6">
                             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                             quasi. In deleniti eaque aut repudiandae et a id nisi.
